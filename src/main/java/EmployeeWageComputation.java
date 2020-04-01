de47b89d15399604182dbdd34e5c8aa8386e7d8a
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-01 14:13:32 +0530

    ADDED:calculated dail employee wage

--- src/main/java/EmployeeWageComputation.java
+++ src/main/java/EmployeeWageComputation.java
@@ -1,26 +1,23 @@
 import java.util.Random;
 public class EmployeeWageComputation
 {
+    //constant
+    public static final int WAGE_PER_HOUR=20;
+    public static final int FULL_DAY_HOUR=8;
+
+    //method calculate daily_employee wage
+    public int calculateDailyWage(int a,int b)
+    {
+        int result=a*b;
+        return result;
+    }
     //main method
     public static void main(String[] args)
     {
-        // create random object
-        Random random=new Random();
-
-        //return value between 0-2
-        int number=random.nextInt(2);
-
-        //print value
-        System.out.println(number);
-
-        //check employee attendance
-        if(number==1)
-        {
-            System.out.println("employee is present");
-        }
-        else
-        {
-            System.out.println("employee is absent");
-        }
+        //create object of class
+        EmployeeWageComputation emp=new EmployeeWageComputation();
+        //call method using object of class
+        int daily_wage=emp.calculateDailyWage(WAGE_PER_HOUR,FULL_DAY_HOUR);
+        System.out.println("daily employee wage:"+daily_wage);
     }
 }

28ce54afdc142eb69ef9f29572053e8f62a9e0e9
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-01 17:28:40 +0530

    ADDED:calculated monthly wage of employee

--- src/main/java/EmployeeWageComputation.java
+++ src/main/java/EmployeeWageComputation.java
@@ -3,37 +3,44 @@ public class EmployeeWageComputation
 {
     //constant
     public static final int WAGE_PER_HOUR = 20;
-    public static final int FULL_TIME = 1;
-    public static final int PART_TIME = 2;
-    public static final int ABSENT = 0;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int IS_ABSENT = 0;
 
     //method created for calculated employee wage
-    public void calculatedEmployeeWage()
+    public void calculatedMonthlyWage()
     {
         //variable
         int employeeHour=0;
-
-        // create random object
-        Random random = new Random();
-
-        //return value between 0-3
-        int choice = random.nextInt(3);
-        switch (choice)
+        int totalSalary=0;
+        int workingDays=20;
+        int salary=0;
+        for(int day=1;day<=workingDays;day++)
         {
-            case FULL_TIME:
-                employeeHour = 8;
-                break;
-            case PART_TIME:
-                employeeHour = 4;
-                break;
-            case ABSENT:
-                employeeHour = 0;
-                break;
-            default:
-                System.out.println("invalid choice");
+            // create random object
+            Random random = new Random();
+            //return value between 0-3
+            int choice = random.nextInt(3);
+            switch (choice)
+            {
+                case IS_FULL_TIME:
+                    employeeHour = 8;
+                    break;
+                case IS_PART_TIME:
+                    employeeHour = 4;
+                    break;
+                case IS_ABSENT:
+                    employeeHour = 0;
+                    break;
+                default:
+                    System.out.println("invalid choice");
+            }
+            //calculated daily salary
+            salary = (WAGE_PER_HOUR * employeeHour);
+            //calculated monthlt salary
+            totalSalary = (totalSalary + salary);
         }
-        int daily_employee_wage=(WAGE_PER_HOUR*employeeHour);
-        System.out.println(daily_employee_wage);
+        System.out.println("total salary" + totalSalary);
     }
     //main method
      public static void main(String args[])
@@ -41,6 +48,6 @@ public class EmployeeWageComputation
          //create object of class
          EmployeeWageComputation emp = new EmployeeWageComputation();
          //method call
-         emp.calculatedEmployeeWage();
+         emp.calculatedMonthlyWage();
      }
 }

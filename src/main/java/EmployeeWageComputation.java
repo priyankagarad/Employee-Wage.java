ea31f368dfb4d2b921c211ed973f6b623dd7ae04
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-01 12:44:45 +0530

    ADDED:using random function check employee attendance

--- src/main/java/EmployeeWageComputation.java
+++ src/main/java/EmployeeWageComputation.java
@@ -1,5 +1,26 @@
-public class EmployeeWageComputation {
-    public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation");
+import java.util.Random;
+public class EmployeeWageComputation
+{
+    //main method
+    public static void main(String[] args)
+    {
+        // create random object
+        Random random=new Random();
+
+        //return value between 0-2
+        int number=random.nextInt(2);
+
+        //print value
+        System.out.println(number);
+
+        //check employee attendance
+        if(number==1)
+        {
+            System.out.println("employee is present");
+        }
+        else
+        {
+            System.out.println("employee is absent");
+        }
     }
 }

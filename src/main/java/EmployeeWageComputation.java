912753cf2d028782c5e8d3e692303ed0ff4869a4
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-01 21:12:28 +0530

    ADDED:calculated daily wage for month and total monthly wage

--- src/main/java/EmployeeWageComputation.java
+++ src/main/java/EmployeeWageComputation.java
@@ -8,6 +8,7 @@ public class EmployeeWageComputation
     public static final int IS_ABSENT = 0;
     public static final int MAX_WORKING_HOURS = 100;
     public static final int MAX_DAYS = 20;
+
     // method created
     public int getWorkHours(int choice)
     {
@@ -37,21 +38,37 @@ public class EmployeeWageComputation
         int totalWorkingDays=0;
         int employeeHour=0;
         int totalSalary=0;
+        int totalMonthlyWage=0;
         while (totalWorkingDays<MAX_DAYS && totalEmployeeHour<MAX_WORKING_HOURS)
         {
             // create random object
             Random random = new Random();
             //generate value between 0-3
             int choice = random.nextInt(3);
-            //call method
+            //method call
             employeeHour=getWorkHours(choice);
             //calculated total employee hours
             totalEmployeeHour = (totalEmployeeHour + employeeHour);
-            //calculated total salary
+
+            //calculated daily employee hour for month
+            int array[]=new int[50];
             totalWorkingDays++;
+            array[totalWorkingDays]=dailyWage(employeeHour);
+            System.out.println(array[totalWorkingDays]);
+
+            //calculated total monthly wage
+            totalMonthlyWage=(totalMonthlyWage+array[totalWorkingDays]);
         }
+        System.out.println(totalMonthlyWage);
         totalSalary = (totalEmployeeHour *WAGE_PER_HOUR);
         System.out.println("total salary" + totalSalary);
+    }
+    //create method for daily employee wage calculation
+    public int dailyWage(int employeeHour)
+    {
+        int daily_Wage=0;
+        daily_Wage=(WAGE_PER_HOUR*employeeHour);
+        return daily_Wage;
     }
      //main method
      public static void main(String args[])
